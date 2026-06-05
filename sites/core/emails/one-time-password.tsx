56399baf41be84a50111147f007f8645857e0d06
Rom Vales Villanueva <rvvillanueva.rbpi@gmail.com>
2026-06-05 16:39:23 +0800

    Added react-email and resend for prompting existing users their OTP.

---
+++ sites/core/emails/one-time-password.tsx
@@ -0,0 +1,19 @@
+
+import { PropsWithChildren } from 'react';
+import { Html, Head, Body, Tailwind } from 'react-email'
+
+export default function OneTimePassword() {
+
+  return (
+    <Tailwind>
+      <Html lang="en">
+      <Head>
+
+      </Head>
+      <Body>
+        
+      </Body>
+      </Html>
+    </Tailwind>
+  )
+}
